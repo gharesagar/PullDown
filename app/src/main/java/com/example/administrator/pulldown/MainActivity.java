77b668330a7f48c002ce56f6e4f25a3d6fea3212
Sagar <gharesagar80@gmail.com>
2019-09-12 17:44:24 +0530

    add boolean method

--- app/src/main/java/com/example/administrator/pulldown/MainActivity.java
+++ app/src/main/java/com/example/administrator/pulldown/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.pulldown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
         String testConflicts="TestConflicts";
     }
 
-    private void abcl(){
-
+    private void test(){
+        
     }
 
 }
